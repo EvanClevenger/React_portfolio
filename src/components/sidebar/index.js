45d3836bd21ccfadaa2bbcc09c35f24289cb7749
Evan Clevenger <evanclevenger@gmail.com>
2023-06-10 11:09:19 -0500

    added styling and links to sidebar

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -2,8 +2,8 @@ import './index.scss'
 import  {Link, NavLink} from 'react-router-dom'
 import LogoS from "../../assets/images/mario logo.png"
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faHome, faUser, faEnvelope} from '@fortawesome/free-solid-svg-icons'
-
+import {faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faLinkedin, faGithub} from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => (
     <div className='nav-bar'>
@@ -23,6 +23,25 @@ const Sidebar = () => (
                 <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e' />
             </NavLink>
         </nav>
+        <ul>
+            <li>
+                <a target="_blank" 
+                rel="noreferrer" 
+                href='https://www.linkedin.com/in/evan-clevenger-436457216/'
+                >
+                <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e"/>
+                </a>
+            </li>
+
+            <li>
+                <a target="_blank" 
+                rel="noreferrer" 
+                href='https://github.com/EvanClevenger'
+                >
+                <FontAwesomeIcon icon={faGithub} color="#4d4d4e"/>
+                </a>
+            </li>
+        </ul>
 
     </div>
 )
