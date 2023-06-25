2aa1b46971092881ea954f4d432bba86f72cb455
EvanClevenger <102931271+EvanClevenger@users.noreply.github.com>
2023-06-25 15:26:54 -0500

    added rubberband animation to home, issue with useEffect

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,23 +1,25 @@
 import LogoTitle from '../../assets/images/e.png'
 import { Link } from 'react-router-dom'
 import './index.scss'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import AnimatedLetters from '../AnimatedLetters'
 
 const Home = () => {
-    const {letterClass, setLetterClass} = useState('text-animate')
+    const [letterClass, setLetterClass] = useState('text-animate')
     const nameArray = ['v', 'a', 'n',' ', 'C','l','e','v','e','n','g','e','r']
-    const jobArray=['W','e','b',' ', 'D','e','v','e','l','o','p','e','r']
+    const jobArray=['W','e','b',' ', 'D','e','v','e','l','o','p','e','r','!']
+
+  
 
     return(
         <div className=" container home-page"> 
          <div className="text-zone">
                 <h1>
-                <span class Name={letterClass}>H</span>
-                <span class Name={`${letterClass} _12`}>i</span>
+                <span className={letterClass}>H</span>
+                <span className={`${letterClass} _12`}>i,</span>
                 <br /> 
-                <span class Name={`${letterClass} _13`}>I</span>
-                <span class Name={`${letterClass} _14`}>'m</span>
+                <span className={`${letterClass} _13`}>I</span>
+                <span className={`${letterClass} _14`}>'m</span>
 
                 <img src={LogoTitle} alt="developer" />
 
